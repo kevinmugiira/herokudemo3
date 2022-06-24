07d3b225dbe06069022ae970698f74157fdffb85
Kevin Mugiira <kevinmugiira@gmail.com>
2022-06-24 14:51:27 +0300

    changed some

---
+++ src/test/java/com/example/heroku2demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heroku2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
